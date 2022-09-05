87f26a6c791f2f8e24dd5afbbc6ea9b54a49711c
Vamsieswar <112846315+Vamsieswar@users.noreply.github.com>
2022-09-05 19:01:58 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,13 @@
+# author vamsieswar 474 05/09/2022
+num1=input("enter num1:")
+num2=input("enter num2:")
+num3=input("enter num3:")
+print("the greatest number is")
+max=0
+if num1>=num2 and num1>=num3:
+    print(num1)
+elif num2>=num1 and num2>=num3:
+    print(num2)
+else:
+    print(num3)
+
